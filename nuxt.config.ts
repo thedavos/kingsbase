b494655b7c91f3f34d9fa92ad018d5421bf534be
David <david.vargas@global66.com>
2024-12-12 18:16:30 -0500

    chore: updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
@@ -8,20 +10,56 @@ export default defineNuxtConfig({
   // https://nuxt.com/modules
   modules: [
     '@nuxthub/core',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    'nuxt-auth-utils',
     '@nuxt/eslint'
   ],
+
+  alias: {
+    server: fileURLToPath(new URL('./server/', import.meta.url)),
+    root: fileURLToPath(new URL('./', import.meta.url)),
+  },
+
   hub: {
     database: true,
     kv: true,
     blob: true,
     cache: true,
   },
+
+  hooks: {
+    'nitro:build:before'(nitro) {
+      nitro.options.moduleSideEffects.push('reflect-metadata');
+    },
+  },
+
   nitro: {
     experimental: {
       // Enable Server API documentation within NuxtHub
-      openAPI: true
-    }
+      openAPI: true,
+      tasks: true,
+    },
+    esbuild: {
+      options: {
+        tsconfigRaw: {
+          compilerOptions: {
+            experimentalDecorators: true,
+          },
+        },
+      },
+    },
   },
   // Development
   devtools: { enabled: true },
+
+  // Development config
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        commaDangle: 'always-multiline',
+      },
+    },
+  },
 })
