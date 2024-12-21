cc0a23f45686ce38fa3bc7ed930ade6c3cb161be
David <david.vargas@global66.com>
2024-12-20 20:06:39 -0500

    feat(statusCode): added 429 code on map

--- server/utils/statusCode.ts
+++ server/utils/statusCode.ts
@@ -10,5 +10,6 @@ export const statusCodes: Record<number, string> = {
   401: 'Unauthorized',
   403: 'Forbidden',
   404: 'Not Found',
+  429: 'Too Many Requests',
   500: 'Internal Server Error',
 };
